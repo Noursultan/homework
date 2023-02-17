0cbe7face467846af1322277c655422fb79cd9b5
Noursultan <86630166+Noursultan@users.noreply.github.com>
2023-02-17 20:40:26 +0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const tags = ['li', 'span', 'div', 'span', 'h3', 'a', 'div', 'span', 'a', 'li', 'div', 'div'];
-const count = {};
-
-for (const tag of tags) {
-  count[tag] = (count[tag] ? count[tag] + 1 : 1);
-}
-
-console.log(count);
