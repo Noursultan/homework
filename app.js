521393cc0f2633b526b6ae1fba4b2e879baf9df0
Noursultan <86630166+Noursultan@users.noreply.github.com>
2023-02-24 23:52:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function add() {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  var result = num1 + num2;
+  document.getElementById("result").innerHTML = result;
+}
+
+function subtract() {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  var result = num1 - num2;
+  document.getElementById("result").innerHTML = result;
+}
+
+function multiply() {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  var result = num1 * num2;
+  document.getElementById("result").innerHTML = result;
+}
